10600daac87df4f43050908c22ea97ddde7bb084
jjeongneee <gardening0530@gmail.com>
2023-11-13 12:27:01 +0900

    회원 정보 수정 기능

--- src/main/java/com/example/prj1be20231109/controller/MemberController.java
+++ src/main/java/com/example/prj1be20231109/controller/MemberController.java
@@ -83,6 +83,16 @@ public class MemberController {
         }
 
         return ResponseEntity.internalServerError().build();
-
     }
+
+    @PutMapping("edit")
+    public ResponseEntity edit(@RequestBody Member member) {
+        // TODO: 로그인 했는지? 자기 정보인지?
+        if (service.update(member)) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
 }

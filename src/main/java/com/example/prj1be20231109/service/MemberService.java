d8959a975ad7a3f7d676680482021819b9479984
jjeongneee <gardening0530@gmail.com>
2023-11-16 14:27:13 +0900

    댓글 있는 멤버 탈퇴 기능
    + 어드민 인지 확인 (isAdmin)을
      MeberService에서 Member로 옮김

--- src/main/java/com/example/prj1be20231109/service/MemberService.java
+++ src/main/java/com/example/prj1be20231109/service/MemberService.java
@@ -3,6 +3,7 @@ package com.example.prj1be20231109.service;
 import com.example.prj1be20231109.domain.Auth;
 import com.example.prj1be20231109.domain.Member;
 import com.example.prj1be20231109.mapper.BoardMapper;
+import com.example.prj1be20231109.mapper.CommentMapper;
 import com.example.prj1be20231109.mapper.MemberMapper;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -16,24 +17,25 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class MemberService {
-    // 의존성 주입:
-    //데이터베이스 작업을 위해 MemberMapper를 사용합니다.
+
     private final MemberMapper mapper;
     private final BoardMapper boardMapper;
+    private final CommentMapper commentMapper;
+    private final BoardService boardService;
 
-    // add: 새로운 멤버를 추가합니다.
     public boolean add(Member member) {
         return mapper.insert(member) == 1;
     }
-    // getId 및 getEmail: 주어진 ID 또는 이메일을 가진 멤버가 이미 존재하는지 확인합니다.
+
     public String getId(String id) {
         return mapper.selectId(id);
     }
 
     public String getEmail(String email) {
         return mapper.selectEmail(email);
+
     }
-    // validate: 멤버 객체를 유효성 검사합니다.
+
     public boolean validate(Member member) {
         if (member == null) {
             return false;
@@ -52,22 +54,29 @@ public class MemberService {
         }
         return true;
     }
-    // list: 모든 멤버의 목록을 가져옵니다.
+
     public List<Member> list() {
         return mapper.selectAll();
     }
 
-    // 멤버 정보 가져오기 및 삭제:
-    //getMember 메서드는 특정 멤버의 정보를 가져옵니다.
     public Member getMember(String id) {
         return mapper.selectById(id);
     }
-    // deleteMember 메서드는 멤버를 삭제합니다.
-    public boolean deleteMember(String id) {
-        // 1. 이 멤버가 작성한 게시물 삭제
-         boardMapper.deleteByWriter(id);
 
-        // 2. 이 멤버 삭제
+
+    public boolean deleteMember(String id) {
+        // 이 멤버가 작성한 댓글 삭제
+        commentMapper.deleteByMemberId(id);
+
+        // 이 멤버가 작성한 게시물 삭제
+
+        //   이 멤버가 작성한 게시물 번호들 조회
+        List<Integer> boardIdList = boardMapper.selectIdListByMemberId(id);
+
+        //   게시물 번호들 loop 각 게시물 삭제(boardService.remove)
+        boardIdList.forEach((boardId) -> boardService.remove(boardId));
+
+        // 이 멤버 삭제
 
         return mapper.deleteById(id) == 1;
     }
@@ -80,7 +89,7 @@ public class MemberService {
 //        }
 
         return mapper.update(member) == 1;
-        
+
     }
 
     public String getNickName(String nickName) {
@@ -90,9 +99,10 @@ public class MemberService {
     public boolean login(Member member, WebRequest request) {
         Member dbMember = mapper.selectById(member.getId());
 
-        if (dbMember != null) {
 
-            if (dbMember.getPassword().equals(member.getPassword())){
+        if (dbMember != null) {
+            if (dbMember.getPassword().equals(member.getPassword())) {
+
                 List<Auth> auth = mapper.selectAuthById(member.getId());
                 dbMember.setAuth(auth);
 
@@ -101,6 +111,7 @@ public class MemberService {
                 return true;
             }
         }
+
         return false;
     }
 
